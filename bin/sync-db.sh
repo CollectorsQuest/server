1b543925291d083aa9131244e68ccb3e0ef274c3
Kiril Angov <kiril.angov@gmail.com>
2012-02-24 09:34:00 -0500

    Adding support for compression in sync-db.sh

--- bin/sync-db.sh
+++ bin/sync-db.sh
@@ -4,17 +4,17 @@ mysqladmin drop collectorsquest_dev && mysqladmin create collectorsquest_dev
 
 echo "Syncing collectorsquest_com..."
 mysqldump -h master.cwvkronpbfrx.us-east-1.rds.amazonaws.com -usync -p4muNB6D47ACvfa \
---skip-opt --quick --extended-insert --create-options --single-transaction --set-charset \
+--skip-opt --compress --quick --extended-insert --create-options --single-transaction --set-charset \
 collectorsquest_com | mysql -u root -D collectorsquest_dev
 
 echo "Syncing collectorsquest_blog..."
 mysqldump -h master.cwvkronpbfrx.us-east-1.rds.amazonaws.com -usync -p4muNB6D47ACvfa \
---skip-opt --quick --extended-insert --create-options --single-transaction --set-charset \
+--skip-opt --compress --quick --extended-insert --create-options --single-transaction --set-charset \
 --ignore-table=collectorsquest_blog.propel_migration collectorsquest_blog | mysql -u root -D collectorsquest_dev
 
 echo "Syncing collectorsquest_queue..."
 mysqldump -h master.cwvkronpbfrx.us-east-1.rds.amazonaws.com -usync -p4muNB6D47ACvfa \
---skip-opt --quick --extended-insert --create-options --single-transaction --set-charset \
+--skip-opt --compress --quick --extended-insert --create-options --single-transaction --set-charset \
 --ignore-table=collectorsquest_queue.propel_migration collectorsquest_queue | mysql -u root -D collectorsquest_dev
 
 echo "Fixing collectorsquest_dev.wp_options..."
