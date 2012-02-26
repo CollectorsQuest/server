203c5c6f53f20a9be274d2641db9f52a64ed06a6
Kiril Angov <kiril.angov@gmail.com>
2012-02-26 21:58:56 +0200

    Adding one more perl module to the cookbooks for mod_perl

--- etc/cookbooks/zend/recipes/modperl.rb
+++ etc/cookbooks/zend/recipes/modperl.rb
@@ -4,3 +4,7 @@ package "perl-CPAN"
 execute "Install Digest::HMAC_SHA1" do
   command "/usr/bin/cpan Digest::HMAC_SHA1"
 end
+
+execute "Install Digest::SHA1" do
+  command "/usr/bin/cpan Digest::SHA1"
+end
