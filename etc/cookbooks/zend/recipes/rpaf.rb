3fcd24601355c51565cb602676eb6b9a75a6b9b6
root <root@ip-10-196-26-143.ec2.internal>
2012-06-23 18:44:33 -0400

    Adding Chef recipe for mod_rpaf

---
+++ etc/cookbooks/zend/recipes/rpaf.rb
@@ -0,0 +1,24 @@
+include_recipe "zend"
+package "httpd-devel"
+
+remote_file "/tmp/mod_rpaf-0.6.tar.gz" do
+  source "http://www.linuxservices.co.uk/mod_rpaf-0.6.tar.gz"
+  not_if { ::File.exists?("/tmp/mod_rpaf-0.6.tar.gz") }
+end
+
+remote_file "/tmp/mod_rpaf_xforward_for.patch" do
+  source "http://www.linuxservices.co.uk/mod_rpaf_xforward_for.patch"
+  not_if { ::File.exists?("/tmp/mod_rpaf_xforward_for.patch") }
+end
+
+execute "Extract mod_xsendfile source" do
+  cwd "/tmp"
+  command "tar -zxf /tmp/mod_rpaf-0.6.tar.gz"
+  not_if { ::File.exists?("/tmp/mod_rpaf-0.6") }
+end
+
+bash "Build and Install mod_rpaf" do
+  cwd "/tmp/mod_rpaf-0.6"
+  code "patch < /tmp/mod_rpaf_xforward_for.patch && apxs -i -c -n mod_rpaf-2.0.so mod_rpaf-2.0.c"
+  not_if { ::File.exists?("/usr/lib/httpd/modules/mod_rpaf-2.0.so") }
+end
