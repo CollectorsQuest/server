363f132adcb9d20f53f1f2a065126d24849fe470
root <root@web-02.(none)>
2012-11-01 18:39:58 +0000

    Add bin/ini and internal IP change for web-01

--- etc/cookbooks/production/recipes/nfs.rb
+++ etc/cookbooks/production/recipes/nfs.rb
@@ -3,7 +3,7 @@ package "rpcbind"
 
 bash "Mount NFS Shares" do
   code <<-EOH
-    mount -t nfs 10.196.26.143:/www/vhosts/collectorsquest.com/shared/uploads /www/vhosts/collectorsquest.com/shared/uploads
-    # mount -t nfs 10.196.26.143:/www/vhosts/collectorsquest.com/shared/secure /www/vhosts/collectorsquest.com/shared/secure
+    mount -t nfs 10.195.183.239:/www/vhosts/collectorsquest.com/shared/uploads /www/vhosts/collectorsquest.com/shared/uploads
+    # mount -t nfs 10.195.183.239:/www/vhosts/collectorsquest.com/shared/secure /www/vhosts/collectorsquest.com/shared/secure
   EOH
 end
