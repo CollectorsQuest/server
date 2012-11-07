4cc46f8f574edcc8c709d7060711f75897ea73eb
Kiril Angov <kangov@collectorsquest.com>
2012-11-07 11:51:27 -0500

    Fix the browscap recipe

--- etc/cookbooks/zend/recipes/browscap.rb
+++ etc/cookbooks/zend/recipes/browscap.rb
@@ -4,7 +4,8 @@ template "/usr/local/zend/etc/conf.d/browscap.ini" do
   source "browscap.ini"
   mode "0664"
 end
-remote_file "/usr/local/zend/etc/conf.d/lite_php_browscap.txt" do
-  source "http://browsers.garykeith.com/stream.asp?Lite_PHP_BrowsCapINI"
+
+template "/usr/local/zend/etc/conf.d/lite_php_browscap.txt" do
+  source "lite_php_browscap.txt"
   mode "0664"
 end
