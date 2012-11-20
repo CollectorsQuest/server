0a2664f5f21b58c5812df6e818151292c6febdc2
Kiril Angov <kangov@collectorsquest.com>
2012-11-20 14:46:10 -0500

    Fix the chef revipe for xhprof

--- etc/cookbooks/zend/recipes/xhprof.rb
+++ etc/cookbooks/zend/recipes/xhprof.rb
@@ -1,14 +1,14 @@
 include_recipe "zend"
 
 remote_file "/tmp/xhprof.tar.gz" do
-  source "https://nodeload.github.com/preinheimer/xhprof/tarball/01b6c75"
+  source "https://github.com/preinheimer/xhprof/archive/58ceef1a59e89eb44a932e767d04e2340521cd77.tar.gz"
   not_if { ::File.exists?("/tmp/xhprof.tar.gz") }
 end
 
 bash "Installing xhprof.so" do
   cwd "/tmp"
   code <<-EOH
-    tar xfz /tmp/xhprof.tar.gz && cd /tmp/preinheimer-xhprof-01b6c75/extension
+    tar xfz /tmp/xhprof.tar.gz && cd /tmp/xhprof-58ceef1a59e89eb44a932e767d04e2340521cd77/extension
     /usr/local/zend/bin/phpize && ./configure --with-php-config=/usr/local/zend/bin/php-config
     make && make install
   EOH
